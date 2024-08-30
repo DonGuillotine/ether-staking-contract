752bb0b6ea32894fd3790d75acade39ab40924b9
DonGuillotine <infect3dlab@gmail.com>
2024-08-30 14:04:11 +0100

    Feat: Implement variables and mappings for Ether Staking

--- contracts/EtherStaking.sol
+++ contracts/EtherStaking.sol
@@ -5,5 +5,20 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EtherStaking is ReentrancyGuard, Ownable {
-    
+    struct Stake {
+        uint256 amount;
+        uint256 timestamp;
+        bool claimed;
+    }
+
+    mapping(address => Stake) public stakes;
+    mapping(address => uint256) public totalRewards;
+
+    uint256 public constant REWARD_RATE = 1; 
+    uint256 public constant REWARD_PERIOD = 1 days;
+    uint256 public constant MINIMUM_STAKING_PERIOD = 7 days;
+
+    constructor() Ownable(msg.sender) {
+
+    }
 }
